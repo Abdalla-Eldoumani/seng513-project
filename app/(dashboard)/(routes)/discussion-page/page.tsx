6c4f6edb4c9bedc15c3fd297c26a492cd2743d49
Abdalla Eldoumani <aamsdoumani@gmail.com>
2024-12-08 00:34:31 -0700

    feat: update discussion page to include clubId in route parameters

--- app/(dashboard)/(routes)/discussion-page/page.tsx
+++ app/(dashboard)/(routes)/discussion-page/page.tsx
@@ -1,10 +1,10 @@
 import DiscussionPage from "@/components/discussions/discussion-page/main";
 import { GeneralProtectedRoute } from "@/components/general-protected-route";
 
-export default function Page({ params }: { params: { discussionId: string } }) {
+export default function Page({ params }: { params: { discussionId: string, clubId: string } }) {
   return (
     <GeneralProtectedRoute>
-      <DiscussionPage discussionId={params.discussionId} />
+      <DiscussionPage discussionId={params.discussionId} clubId={params.clubId} />
     </GeneralProtectedRoute>
   );
 }
